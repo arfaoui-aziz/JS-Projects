42cce0d140c5d977c43ec158c62bbb96803a4401
arfaoui-aziz <aziz.arfaou.98@gmail.com>
2021-05-21 18:00:21 +0100

    Max Character Occurence s2

--- JS-Cardio-S1/maxCharacter.js
+++ JS-Cardio-S1/maxCharacter.js
@@ -1,21 +1,40 @@
 //Return the character that is most common in a string
 
 const maxCharacter = (str) => {
-  let max = 0;
-  let char = "";
-  let nbrOccur = 0;
+  //* 1st Solution
+  //   let max = 0;
+  //   let char = "";
+  //   let nbrOccur = 0;
 
-  for (let i = 0; i < str.length; i++) {
-    nbrOccur = 0;
-    for (let j = i; j < str.length; j++) {
-      if (str[i] === str[j]) nbrOccur++;
-    }
-    if (max < nbrOccur) {
-      max = nbrOccur;
-      char = str[i];
+  //   for (let i = 0; i < str.length; i++) {
+  //     nbrOccur = 0;
+  //     for (let j = i; j < str.length; j++) {
+  //       if (str[i] === str[j]) nbrOccur++;
+  //     }
+  //     if (max < nbrOccur) {
+  //       max = nbrOccur;
+  //       char = str[i];
+  //     }
+  //   }
+  //   return { max, char };
+
+  //* 2nd Solution
+
+  const charMap = {};
+  let maxNb = 0;
+  let maxChar = "";
+  str.split("").forEach((char) => {
+    charMap[char] ? charMap[char]++ : (charMap[char] = 1);
+  });
+
+  for (let ch in charMap) {
+    if (charMap[ch] > maxNb) {
+      maxNb = charMap[ch];
+      maxChar = ch;
     }
   }
-  return { max, char };
+
+  return { [maxChar]: maxNb };
 };
 
 const output = maxCharacter("javascript");
