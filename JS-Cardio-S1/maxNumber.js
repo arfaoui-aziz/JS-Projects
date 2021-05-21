578d2c5dd18735901569758e6884f35049f31878
arfaoui-aziz <aziz.arfaou.98@gmail.com>
2021-05-21 18:17:59 +0100

    Max Number Occurence in Array

---
+++ JS-Cardio-S1/maxNumber.js
@@ -0,0 +1,21 @@
+const maxNumber = (nbrArray) => {
+  const newNbrArray = nbrArray.join("").split("");
+  const nbrMap = {};
+  let nbrOccur = 0;
+  let nbrMax = 0;
+
+  newNbrArray.forEach((nbr) =>
+    nbrMap[nbr] ? nbrMap[nbr]++ : (nbrMap[nbr] = nbrMap[nbr] = 1)
+  );
+
+  for (number in nbrMap) {
+    if (nbrMap[number] > nbrOccur) {
+      nbrOccur = nbrMap[number];
+      nbrMax = number;
+    }
+  }
+  return { [nbrMax]: nbrOccur };
+};
+
+const output = maxNumber([0, 1, 2, 22, 12, 120]);
+console.log(output);
