c4684e8201a67d8a725f421a4d04ccc245b18a32
arfaoui-aziz <aziz.arfaou.98@gmail.com>
2021-06-11 16:40:15 +0100

    anagrams

---
+++ Algo-DS/problemSolvingPatterns/anagrams.js
@@ -0,0 +1,25 @@
+const isAnagram = (str1, str2) => {
+  if (str1.length !== str2.length) return false;
+  const charCounter = {};
+  //const freqCounter2 = {}
+
+  for (char of str1) {
+    charCounter[char] = (charCounter[char] || 0) + 1;
+  }
+  for (char of str2) {
+    // freqCounter2[char] = (freqCounter2[char] || 0) + 1;
+    if (!charCounter[char]) {
+      return false;
+    } else {
+      charCounter[char] -= 1;
+    }
+  }
+  //   for (key in charCounter) {
+  //     if (!(key in freqCounter2)) return false;
+  //     if (charCounter[key] !== freqCounter2[key]) return false;
+  //   }
+  return true;
+};
+
+const output = isAnagram("cinema", "iceman");
+console.log(output);
