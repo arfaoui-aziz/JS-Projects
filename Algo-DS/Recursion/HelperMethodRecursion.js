c043f466882d39a387b40fbdd2e0ce8636a3554b
arfaoui-aziz <aziz.arfaou.98@gmail.com>
2021-06-15 19:08:53 +0100

    Helper Method Recursion

---
+++ Algo-DS/Recursion/HelperMethodRecursion.js
@@ -0,0 +1,27 @@
+//* Pattern
+// const outer = (input) => {
+//   let outerScopedVariable = [];
+//   const helper = (helperInput) => {
+//     //modify the outerScopedVariable
+//     helper(helperInput--);
+//   };
+//   helper(input);
+//   return outerScopedVariable;
+// };
+
+const countOddsInArray = (array) => {
+  let oddsArray = [];
+  const helper = (helperInput) => {
+    if (helperInput.length === 0) return;
+    if (helperInput[0] % 2 !== 0) oddsArray.push(helperInput[0]);
+    helper(helperInput.slice(1));
+  };
+  helper(array);
+  return oddsArray;
+};
+
+const output = countOddsInArray([
+  1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15,
+]);
+
+console.log(output);
