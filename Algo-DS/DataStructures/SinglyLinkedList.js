61dc36fe68ac008774a29b347d0685f0615e236d
arfaoui-aziz <aziz.arfaou.98@gmail.com>
2021-06-28 09:43:24 +0100

    pushing to a Singly Linked List

--- Algo-DS/DataStructures/SinglyLinkedList.js
+++ Algo-DS/DataStructures/SinglyLinkedList.js
@@ -1,25 +1,27 @@
 class Node {
-  construct(val, next) {
+  constructor(val, next) {
     this.val = val;
     this.next = null;
   }
 }
 
 class SinglyLinkedList {
-  construct() {
+  constructor() {
     this.head = null;
     this.tail = null;
     this.length = 0;
   }
   push(value) {
-    if (this.length === 0) {
-      this.head = new Node(value);
-      this.tail = new Node(value);
+    const newNode = new Node(value);
+    if (!this.head) {
+      this.head = newNode;
+      this.tail = newNode;
     } else {
-      this.tail.next = value;
-      this.tail = new Node(value);
+      this.tail.next = newNode;
+      this.tail = newNode;
     }
-    length++;
+    this.length++;
+    return this;
   }
 }
 
@@ -30,3 +32,11 @@ const list = new SinglyLinkedList();
 // first.next.next = new Node("how");
 // first.next.next.next = new Node("are");
 // first.next.next.next.next = new Node("you");
+
+console.log(list.push(0));
+console.log(list.push(2));
+console.log(list.push(4));
+// console.log(list.head);
+// console.log(list.head.next);
+
+console.log(list);
