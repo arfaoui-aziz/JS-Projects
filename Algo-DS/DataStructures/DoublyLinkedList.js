2acdcc685ce3c18647ac1068afbead6b80375d35
arfaoui-aziz <aziz.arfaou.98@gmail.com>
2021-06-30 15:34:05 +0100

    Push Doubly Linked List

--- Algo-DS/DataStructures/DoublyLinkedList.js
+++ Algo-DS/DataStructures/DoublyLinkedList.js
@@ -12,4 +12,31 @@ class DoublyLinkedList {
     this.tail = null;
     this.length = 0;
   }
+
+  push(val) {
+    const newNode = new Node(val);
+    if (this.length === 0) {
+      this.head = newNode;
+      this.tail = this.head;
+    } else {
+      //*correct
+      //   const oldTail = this.tail;
+      //   oldTail.next = newNode;
+      //   this.tail = newNode;
+      //   this.tail.previous = oldTail;
+
+      //!shorter
+      this.tail.next = newNode;
+      newNode.previous = this.tail;
+      this.tail = newNode;
+    }
+    this.length++;
+    return this;
+  }
 }
+
+const list = new DoublyLinkedList();
+list.push(4);
+list.push(3);
+list.push(1);
+console.log(list.push(50));
