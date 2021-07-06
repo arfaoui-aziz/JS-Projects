c7a9b56dcea30f8d9767ba7d694dcc4c0939c646
arfaoui-aziz <aziz.arfaou.98@gmail.com>
2021-07-06 13:17:41 +0100

    Binary Heap Extract Max

--- Algo-DS/DataStructures/BinaryHeap.js
+++ Algo-DS/DataStructures/BinaryHeap.js
@@ -1,6 +1,7 @@
 class MaxBinaryHeap {
   constructor() {
-    this.values = [];
+    // this.values = [190, 60, 100, 50, 29, 30, 14, 6, 15, 1];
+    this.values = [55];
   }
   //   ! Create a MaxBinaryHeap from an array
   create(arr) {
@@ -27,7 +28,7 @@ class MaxBinaryHeap {
     this.values.push(val);
     let index = this.values.length - 1;
     let parent = Math.floor((index - 1) / 2);
-    while (this.values[index] > this.values[parent]) {
+    while (val > this.values[parent]) {
       [this.values[index], this.values[parent]] = [
         this.values[parent],
         this.values[index],
@@ -37,9 +38,53 @@ class MaxBinaryHeap {
     }
     return this;
   }
+
+  //remove root (sink Down)
+  extractMax() {
+    //Swap
+    let max = this.values[0];
+    let val = this.values.pop();
+    //! Edge Case if we have only one element in the array we pop the value
+    //! and reassign it to the first element again so we never have an empty Heap
+    //! we always have at least one element in our Heap
+    if (this.values.length === 0) return max;
+    this.values[0] = val;
+    //Sink Down
+    let index = 0;
+    let leftIdx = 0;
+    let rightIdx = 0;
+    let leftChild = 0;
+    let rightChild = 0;
+    let largestChild = 0;
+    while (true) {
+      leftIdx = index * 2 + 1;
+      rightIdx = index * 2 + 2;
+
+      if (leftIdx < this.values.length) leftChild = this.values[leftIdx];
+      if (rightIdx < this.values.length) rightChild = this.values[rightIdx];
+
+      largestChild = leftChild > rightChild ? leftIdx : rightIdx;
+
+      if (val < this.values[largestChild]) {
+        [this.values[index], this.values[largestChild]] = [
+          this.values[largestChild],
+          this.values[index],
+        ];
+
+        index = largestChild;
+      } else {
+        break;
+      }
+    }
+    return max;
+  }
 }
 
 heap = new MaxBinaryHeap();
 
-console.log(heap.create([100, 29, 30, 15, 50, 190, 14, 6]));
-console.log(heap.insert(60));
+// console.log(heap.create([100, 29, 30, 15, 50, 190, 14, 6]));
+
+// console.log(heap.insert(60));
+// console.log(heap.insert(1));
+console.log(heap.extractMax());
+console.log(heap);
