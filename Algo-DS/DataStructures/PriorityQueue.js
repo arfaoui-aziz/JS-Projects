5f0e1ae6577a43fdfcd674dacad9658d4d85a116
arfaoui-aziz <aziz.arfaou.98@gmail.com>
2021-07-08 14:00:30 +0100

    Priority Queue Big O

--- Algo-DS/DataStructures/PriorityQueue.js
+++ Algo-DS/DataStructures/PriorityQueue.js
@@ -12,6 +12,7 @@ class PriorityQueue {
     this.values = [];
   }
 
+  //* O(log n) Insertion
   enqueue(val, priority) {
     const newNode = new Node(val, priority);
     this.values.push(newNode);
@@ -31,6 +32,7 @@ class PriorityQueue {
   }
 
   //get highest priority
+  //* O(log n) Removal
   dequeue() {
     let maxPriority = this.values[0];
     let val = this.values.pop();
@@ -49,6 +51,8 @@ class PriorityQueue {
       if (rightIdx < this.values.length) {
         rightChild = this.values[rightIdx].priority;
         if (rightChild < leftChild) minChildIdx = rightIdx;
+        //else if (rightChild === leftChild) in case we want to add another condition
+        // in case of equal priority
       }
 
       if (!minChildIdx || val.priority <= this.values[minChildIdx].priority)
@@ -62,6 +66,48 @@ class PriorityQueue {
     }
     return maxPriority;
   }
+
+  //! Colt Solution
+  // dequeue(){
+  //     const min = this.values[0];
+  //     const end = this.values.pop();
+  //     if(this.values.length > 0){
+  //         this.values[0] = end;
+  //         this.sinkDown();
+  //     }
+  //     return min;
+  // }
+  // sinkDown(){
+  //     let idx = 0;
+  //     const length = this.values.length;
+  //     const element = this.values[0];
+  //     while(true){
+  //         let leftChildIdx = 2 * idx + 1;
+  //         let rightChildIdx = 2 * idx + 2;
+  //         let leftChild,rightChild;
+  //         let swap = null;
+
+  //         if(leftChildIdx < length){
+  //             leftChild = this.values[leftChildIdx];
+  //             if(leftChild.priority < element.priority) {
+  //                 swap = leftChildIdx;
+  //             }
+  //         }
+  //         if(rightChildIdx < length){
+  //             rightChild = this.values[rightChildIdx];
+  //             if(
+  //                 (swap === null && rightChild.priority < element.priority) ||
+  //                 (swap !== null && rightChild.priority < leftChild.priority)
+  //             ) {
+  //                swap = rightChildIdx;
+  //             }
+  //         }
+  //         if(swap === null) break;
+  //         this.values[idx] = this.values[swap];
+  //         this.values[swap] = element;
+  //         idx = swap;
+  //     }
+  // }
 }
 
 let priorityQueue = new PriorityQueue();
