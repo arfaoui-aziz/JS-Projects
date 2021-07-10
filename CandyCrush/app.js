a6afc632e74fb64807605d5204c5769d5921aad4
arfaoui-aziz <aziz.arfaou.98@gmail.com>
2021-07-10 22:34:34 +0100

    Candy Crash random Color Divs

--- CandyCrush/app.js
+++ CandyCrush/app.js
@@ -3,11 +3,14 @@ const grid = document.querySelector(".grid");
 const width = 8;
 const squares = [];
 
+const candyColors = ["red", "yellow", "green", "orange", "purple", "blue"];
 //Create Board
 
 const createBoard = () => {
   for (let i = 0; i < width * width; i++) {
     const square = document.createElement("div");
+    let randomColor = Math.floor(Math.random() * candyColors.length);
+    square.style.backgroundColor = candyColors[randomColor];
     grid.appendChild(square);
     squares.push(square);
   }
