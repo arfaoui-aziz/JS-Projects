4dd82880e9256934fb435e340c1b04d5e1bac2eb
arfaoui-aziz <aziz.arfaou.98@gmail.com>
2021-07-10 22:59:10 +0100

    Candy Crash Drag & Swap Colors

--- CandyCrush/app.js
+++ CandyCrush/app.js
@@ -1,19 +1,61 @@
-// document.addEventListener('DOMContentLoaded',()=>{})
-const grid = document.querySelector(".grid");
-const width = 8;
-const squares = [];
+document.addEventListener("DOMContentLoaded", () => {
+  const grid = document.querySelector(".grid");
+  const width = 8;
+  const squares = [];
 
-const candyColors = ["red", "yellow", "green", "orange", "purple", "blue"];
-//Create Board
+  const candyColors = ["red", "yellow", "green", "orange", "purple", "blue"];
 
-const createBoard = () => {
-  for (let i = 0; i < width * width; i++) {
-    const square = document.createElement("div");
-    let randomColor = Math.floor(Math.random() * candyColors.length);
-    square.style.backgroundColor = candyColors[randomColor];
-    grid.appendChild(square);
-    squares.push(square);
+  //Create Board
+  const createBoard = () => {
+    for (let i = 0; i < width * width; i++) {
+      const square = document.createElement("div");
+      square.setAttribute("draggable", true);
+      square.setAttribute("id", i);
+      let randomColor = Math.floor(Math.random() * candyColors.length);
+      square.style.backgroundColor = candyColors[randomColor];
+      grid.appendChild(square);
+      squares.push(square);
+    }
+  };
+
+  createBoard();
+
+  //Drag the candies
+
+  let colorBeingDragged,
+    colorBeingReplaced,
+    squareIdBeingDragged,
+    squareIdBeingReplaced;
+
+  squares.forEach((square) => square.addEventListener("dragstart", dragStart));
+  squares.forEach((square) => square.addEventListener("dragend", dragEnd));
+  squares.forEach((square) => square.addEventListener("dragover", dragOver));
+  squares.forEach((square) => square.addEventListener("dragenter", dragEnter));
+  squares.forEach((square) => square.addEventListener("dragleave", dragLeave));
+  squares.forEach((square) => square.addEventListener("drop", dragDrop));
+
+  function dragStart() {
+    colorBeingDragged = this.style.backgroundColor;
+    squareIdBeingDragged = parseInt(this.id);
+    console.log(colorBeingDragged, this.id, "dragstart");
   }
-};
 
-createBoard();
+  function dragOver(e) {
+    e.preventDefault();
+  }
+
+  function dragEnter(e) {
+    e.preventDefault();
+  }
+
+  function dragLeave() {}
+
+  function dragDrop(e) {
+    console.log(this.id, "dragdrop");
+    colorBeingReplaced = this.style.backgroundColor;
+    squareIdBeingReplaced = parseInt(this.id);
+    squares[squareIdBeingDragged].style.backgroundColor = colorBeingReplaced;
+  }
+
+  function dragEnd() {}
+});
